8f1a24ac7b5d535c0fb079b933b9ef7b772b73e5
ZhylaA <zak2010@i.ua>
2022-02-11 16:07:25 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -26,7 +26,9 @@
 //     Для этого добавь в проект и используй библиотеку lodash.throttle.
 import throttle from 'lodash.throttle';
 
-const STORAGE_KEY = 'feedback-form-state';
+const STORAGE_KEY = "feedback-form-state";
+console.log(STORAGE_KEY);
+
 const refs = {
     feedbackForm: document.querySelector('.feedback-form'),
     feedbackFormInput:document.querySelector('.feedback-form input'),
@@ -38,7 +40,7 @@ const refs = {
 refs.feedbackForm.addEventListener('submit', onFormSubmit);
 refs.feedbackForm.addEventListener('input', throttle(onFeedbackFormState, 500));
 
-const saveData = localStorage.getItem('feedback-form-state');
+const saveData = localStorage.getItem('STORAGE_KEY');
 const parsedData = JSON.parse(saveData);
 
 if (parsedData) {
@@ -52,38 +54,32 @@ function onFeedbackFormState() {
     const email = refs.feedbackFormInput.value;
     const message = refs.feedbackFormTextarea.value;
     localStorage.setItem(STORAGE_KEY, JSON.stringify({email,message}));
-}
+};
 
 function onFormSubmit(ev) {
   ev.preventDefault();
   const email = ev.currentTarget.elements.email.value;
+  // console.log(ev.currentTarget.elements.email.value);
   const message = ev.currentTarget.elements.message.value;
   if (!message || !email) {
-    return alert('Поля должны быть заполнены!');
-  }
+    return alert('Заполните поля!');} 
   const obj = {
     message,
     email,
   };
-     console.log(obj);
+  console.log(obj);
   localStorage.removeItem(STORAGE_KEY);
-  formEl.reset();
-}
-    
-    
-    
-    
-// function populateTextarea() {
-//     const savedTextTextarea = localStorage.getItem(STORAGE_KEY);
-//     if (savedTextTextarea) { refs.feedbackFormTextarea.value = savedTextTextarea };
-// };
-// function populateTextInput() {
-//     const savedTextInput = localStorage.getItem(STORAGE_KEY);
-//         if (savedTextInput) {
-//         // console.log(savedTextInput);
-//         refs.feedbackFormInput.value = savedTextInput;
-//     };
-// };
+  refs.feedbackForm.reset();
+};
 
-// // console.log(feedbackFormInput);
-// // При сабмите формы очищай хранилище и поля формы
+function populateTextarea() {
+    const savedTextTextarea = localStorage.getItem(STORAGE_KEY);
+    if (savedTextTextarea) { refs.feedbackFormTextarea.value = savedTextTextarea };
+};
+
+function populateTextInput() {
+    const savedTextInput = localStorage.getItem(STORAGE_KEY);
+        if (savedTextInput) {
+        refs.feedbackFormInput.value = savedTextInput;
+    };
+};
